d191978f952cba3dbc751d1da5888df4cb4d77c8
Josh Mu <hello@joshmu.dev>
2023-06-10 13:11:24 +0800

    astro colorscheme

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   { import = "astrocommunity.pack.typescript" },
