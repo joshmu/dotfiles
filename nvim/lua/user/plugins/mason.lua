95c725effd4023dcd8891bf43ab75cc41e20dffc
Josh Mu <hello@joshmu.dev>
2023-07-02 11:53:08 +1000

    nvim mason & local-highlight

--- nvim/lua/user/plugins/mason.lua
+++ nvim/lua/user/plugins/mason.lua
@@ -15,13 +15,49 @@ return {
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        "stylua",
-      })
-    end,
+    -- opts = function(_, opts)
+    --   -- add more things to the ensure_installed table protecting against community packs modifying it
+    --   opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+    --     "prettier",
+    --     "stylua",
+    --   })
+    -- end,
+    opts = {
+      handlers = {
+        -- for prettier
+        prettier = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+        -- for prettierd
+        prettierd = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+        -- For eslint_d:
+        eslint_d = function()
+          require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".eslintrc.json"
+                or utils.root_has_file ".eslintrc.js"
+            end,
+          })
+        end,
+      },
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
