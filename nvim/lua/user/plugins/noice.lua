f8949d80d00626e6f368845de65149f967b59f4f
Josh Mu <hello@joshmu.dev>
2024-03-02 15:15:38 -0800

    feat: remove noice setup, due to numerous notification errors

--- nvim/lua/user/plugins/noice.lua
+++ nvim/lua/user/plugins/noice.lua
@@ -1,81 +1,84 @@
 -- https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/utility/noice-nvim/init.lua
 
-local utils = require "astronvim.utils"
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed =
-          utils.list_insert_unique(opts.ensure_installed, { "bash", "markdown", "markdown_inline", "regex", "vim" })
-      end
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = function(_, opts)
-      return utils.extend_tbl(opts, {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-        presets = {
-          bottom_search = false, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-        routes = {
-          {
-            -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#hide-search-virtual-text
-            filter = {
-              event = "msg_show",
-              kind = "search_count",
-            },
-            opts = { skip = true },
-          },
-          {
-            -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#hide-written-messages
-            filter = {
-              event = "msg_show",
-              kind = "",
-              find = "written",
-            },
-            opts = { skip = true },
-          },
-          {
-            -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages          {
-            view = "notify",
-            filter = { event = "msg_showmode" },
-          },
-        },
-      })
-    end,
-    init = function() vim.g.lsp_handlers_enabled = false end,
-  },
-  {
-    "folke/edgy.nvim",
-    optional = true,
-    opts = function(_, opts)
-      if not opts.bottom then opts.bottom = {} end
-      table.insert(opts.bottom, {
-        ft = "noice",
-        size = { height = 0.4 },
-        filter = function(_, win) return vim.api.nvim_win_get_config(win).relative == "" end,
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    optional = true,
-    ---@type CatppuccinOptions
-    opts = { integrations = { noice = true } },
-  },
-}
+return {}
+
+-- local utils = require "astronvim.utils"
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = function(_, opts)
+--       if opts.ensure_installed ~= "all" then
+--         opts.ensure_installed =
+--           utils.list_insert_unique(opts.ensure_installed, { "bash", "markdown", "markdown_inline", "regex", "vim" })
+--       end
+--     end,
+--   },
+--   {
+--     "folke/noice.nvim",
+--     event = "VeryLazy",
+--     dependencies = { "MunifTanjim/nui.nvim" },
+--     opts = function(_, opts)
+--       return utils.extend_tbl(opts, {
+--         lsp = {
+--           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--           override = {
+--             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--             ["vim.lsp.util.stylize_markdown"] = true,
+--             ["cmp.entry.get_documentation"] = true,
+--           },
+--         },
+--         presets = {
+--           bottom_search = false, -- use a classic bottom cmdline for search
+--           command_palette = true, -- position the cmdline and popupmenu together
+--           long_message_to_split = true, -- long messages will be sent to a split
+--           inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
+--           lsp_doc_border = false, -- add a border to hover docs and signature help
+--         },
+--         routes = {
+--           {
+--             -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#hide-search-virtual-text
+--             filter = {
+--               event = "msg_show",
+--               kind = "search_count",
+--             },
+--             opts = { skip = true },
+--           },
+--           {
+--             -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#hide-written-messages
+--             filter = {
+--               event = "msg_show",
+--               kind = "",
+--               find = "written",
+--             },
+--             opts = { skip = true },
+--           },
+--           {
+--             -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages          {
+--             view = "notify",
+--             filter = { event = "msg_showmode" },
+--           },
+--         },
+--       })
+--     end,
+--     init = function() vim.g.lsp_handlers_enabled = false end,
+--   },
+--   {
+--     "folke/edgy.nvim",
+--     optional = true,
+--     opts = function(_, opts)
+--       if not opts.bottom then opts.bottom = {} end
+--       table.insert(opts.bottom, {
+--         ft = "noice",
+--         size = { height = 0.4 },
+--         filter = function(_, win) return vim.api.nvim_win_get_config(win).relative == "" end,
+--       })
+--     end,
+--   },
+--   {
+--     "catppuccin/nvim",
+--     optional = true,
+--     ---@type CatppuccinOptions
+--     opts = { integrations = { noice = true } },
+--   },
+-- }
+--
