c4267bde1927dcf8aa225b441a49a61e841e9c0b
Josh Mu <hello@joshmu.dev>
2024-08-19 17:14:51 +1000

    feat: nvim ufo folding

--- nvim/lua/core/plugins/lspconfig.lua
+++ nvim/lua/core/plugins/lspconfig.lua
@@ -156,6 +156,11 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -263,6 +268,8 @@ return {
           end,
         },
       }
+
+      require('ufo').setup()
     end,
   },
 }
---
+++ nvim/lua/custom/plugins/ufo.lua
@@ -0,0 +1,14 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  disable = false,
+  dependencies = 'kevinhwang91/promise-async',
+  config = function()
+    vim.wo.foldlevel = 99 -- feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.wo.foldenable = true
+
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith)
+  end,
+}
