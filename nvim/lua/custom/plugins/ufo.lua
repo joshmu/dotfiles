1a1cc734de93848627106be0baecb8ba8d37bf02
Josh Mu <hello@joshmu.dev>
2024-08-31 12:37:47 +1000

    feat: kitty

--- nvim/lua/custom/plugins/ufo.lua
+++ nvim/lua/custom/plugins/ufo.lua
@@ -3,10 +3,11 @@ return {
   disable = false,
   dependencies = 'kevinhwang91/promise-async',
   config = function()
-    vim.o.foldcolumn = '1'
+    vim.o.foldcolumn = '0'
     vim.wo.foldlevel = 99 -- feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.wo.foldenable = true
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
